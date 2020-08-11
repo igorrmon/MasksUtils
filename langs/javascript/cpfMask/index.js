a19c24ebdc0d0b0e422fd220d4f95b7275ff3958
Igor Ramon <igor.ramon14@gmail.com>
2020-08-11 16:08:08 -0300

    Add CPF mask

---
+++ langs/javascript/cpfMask/index.js
@@ -0,0 +1,8 @@
+function cpfMask(value) {
+    return value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+}
