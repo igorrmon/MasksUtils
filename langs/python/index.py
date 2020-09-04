653fc6538ef92ddcd5a01bdf6662292148b1bbc6
Mateus Fernandes Costa <mateustech686@gmail.com>
2020-09-04 18:33:27 -0300

    [Py] - function mask cpf.

--- langs/python/index.py
+++ langs/python/index.py
@@ -1,9 +1,21 @@
 
+#Valida se o email esta em um formato válido
+ 
 def IsEmail(args):
   x = str(args)
   atpos = x.find("@");
   dotpos = x.find(".");
   if (atpos < 1 or dotpos < atpos + 2 ):
-     return False;
+     pass;
+  else:
+     return True;
 
-  return True;
+#convert from 99999999999 to 999.999.999-99
+def CpfMask(args):
+   x = str(args)
+   if len(x) == 11:
+      x = x.zfill(11)
+      cpf = '{}.{}.{}-{}'.format(x[:3], x[3:6], x[6:9], x[9:])
+      return cpf
+   else:
+      pass
